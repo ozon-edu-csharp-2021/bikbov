3f5cfaf6d330974e352e584990df8851a44b420e
galim <gn.bikbov@physics.msu.ru>
2021-10-21 06:59:57 +0300

    HW1-fix

---
+++ merchandise-service/MerchandiseService/Program.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<MerchandiseService.Startup>(); });
